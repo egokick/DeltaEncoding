15b68addf086e88b85515903672accd06780a99a
Ego <egokick@gmail.com>
2018-08-05 23:05:40 +0100

    complete
    delta compression demo

--- Program.cs
+++ Program.cs
@@ -23,25 +23,38 @@ namespace DeltaEncoding
                 var frame = CreateFrame(r);
                 if (init) lastFrame = frame;
 
+                #region Server side 
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 DisplayFrame(frame);
+                var frameDelta = FrameDelta(frame, lastFrame);
                 Console.WriteLine("Frame Length = " +  frame.Length.ToString() + 
                     " True: " + frame.Count(x=>x).ToString()
                     + " False: " + frame.Count(x => !x).ToString());
+                #endregion
 
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                DisplayFrame(lastFrame);
-                Console.WriteLine("Frame Length = " + lastFrame.Length.ToString() +
-                    " True: " + lastFrame.Count(x => x).ToString()
-                    + " False: " + lastFrame.Count(x => !x).ToString());
+                #region ClientSide
+                Console.ForegroundColor = ConsoleColor.Green;               
+                lastFrame = DisplayDeltaFrame(lastFrame, frameDelta);                
+                Console.WriteLine("Delta Length = " + frameDelta.Count.ToString());
+                #endregion
 
                 Thread.Sleep(1000);
                 Console.Clear();
-
-                lastFrame = frame;
+                
                 if(init) init = false;
             }
+        }
+
+        public static List<int> FrameDelta(bool[] currentFrame, bool[] lastFrame)
+        {
+            List<int> delta = new List<int>();
+
+            for(int i = 0; i < lastFrame.Length; i++)
+            {
+                if (lastFrame[i] != currentFrame[i])
+                    delta.Add(i);
+            }
+            return delta;
         }
 
         public static bool[] CreateFrame(Random r)
@@ -52,10 +65,33 @@ namespace DeltaEncoding
 
             return frame;
         }
-       
+
+        static bool[] DisplayDeltaFrame(bool[] lastFrame, List<int> frameDelta)
+        {
+            var frame = lastFrame;
+
+            // construct new frame 
+            foreach (int i in frameDelta)
+                frame[i] = !lastFrame[i];   
+
+            char[] ascii = new char[FRAMESIZE];
+
+            //convert frame to ascii
+            for (int i = 0; i < FRAMESIZE; i++)
+            {
+                if (frame[i]) ascii[i] = (char)35;
+                else ascii[i] = (char)32;
+            }
+
+            Console.WriteLine(ascii);
+
+            return frame;
+        }
+
         static void DisplayFrame(bool[] frame)
         {
             char[] ascii = new char[FRAMESIZE];
+
             //convert frame to ascii
             for (int i = 0; i < FRAMESIZE; i++)
             {
